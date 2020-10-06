4b3198f9a8f258dcfa604689b658015d4ba0fec9
nofikoffgit <ruslan.novikov@gmail.com>
2020-10-06 16:00:59 +0300

    подправил флаги и логику

--- tree.js
+++ tree.js
@@ -1,20 +1,24 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+
 let sqlArrayTrees = [];
 // основноый вызов
 
 getChildren('https://www.olx.ua/transport/legkovye-avtomobili/')
-// .then((xxx) => {
-//     console.log("ПИШЕМ В БД2\n", xxx);
-// })
+    .then((xxx) => {
+        console.log("ПИШЕМ В БД2\n", xxx);
+        const tree2db = require('./treetomssqldb2');
+        tree2db.save(xxx);
+    })
 
 function getChildren(url_current_node) {
 
     return axios.get(url_current_node,
         {
             // не поддерживаме редирект ловим 301 code и пустую страницу ловим
-            maxRedirects: 0, headers: {
+            maxRedirects: 0,
+            headers: {
                 'authority': 'www.olx.ua',
                 'pragma': 'no-cache',
                 'cache-control': 'no-cache',
@@ -24,10 +28,8 @@ function getChildren(url_current_node) {
                 'sec-fetch-site': 'same-origin',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-dest': 'empty',
-                // ВАЖНО !!!
                 'referer': url_current_node,
                 'accept-language': 'en-UA,en;q=0.9,ru-AU;q=0.8,ru;q=0.7,en-US;q=0.6',
-                //'cookie': 'PHPSESSID=psb0hg31nq2ceekba3cunsm847'
             }
         }
     ).then(
@@ -47,11 +49,9 @@ function getChildren(url_current_node) {
         // инициализация - сначала пустой массив -
         // узел сохраняем в вектор обьект, этот обьект позже передается в базу
         sqlArrayTrees[url_current_node] = [];
-        // выходим - ДЕТЕЙ НЕТ
-        // выходим - ДЕТЕЙ НЕТ
+        console.log("УЗЕЛ", url_current_node)
         // выходим - ДЕТЕЙ НЕТ
         if ($('a.topLink').length === 0) {
-
             console.log("Страницу узла вижу, но детей нет")
 
         } else {
@@ -61,30 +61,25 @@ function getChildren(url_current_node) {
                 let title = $(elem).find('span.link').text();
                 // если не пустой заголовок
                 if (typeof title === 'string') {
-                    console.log("Найден следующий подувроень ребенок",  title);
                     sqlArrayTrees[url_current_node][link] = title;
                 }
             })
         }
 
     })
-    .then(
-        () => {
-            // еси надо нырнуть глбуже
-            // еси надо нырнуть глбуже
-            // еси надо нырнуть глбуже
-            // еси надо нырнуть глбуже
-            if (sqlArrayTrees[url_current_node].length) {
-                let promises = [];
-                console.log(sqlArrayTrees[url_current_node], 'дети для ', url_current_node);
-                for (let key in sqlArrayTrees[url_current_node]) {
-                    promises.push(getChildren(key));
-                    // выходим если ссылки на ads не обнаружены
+        .then(
+            () => {
+                // еси надо нырнуть глбуже
+                if (Object.keys(sqlArrayTrees[url_current_node]).length) {
+                    let promises = [];
+                    for (let key in sqlArrayTrees[url_current_node]) {
+                        promises.push(getChildren(key));
+                        // выходим если ссылки на ads не обнаружены
+                    }
+                    return Promise.all(promises);
                 }
-                return Promise.all(promises);
             }
-        }
-    ).then(() => {
-        return sqlArrayTrees;
-    })
+        ).then(() => {
+            return sqlArrayTrees;
+        })
 }
